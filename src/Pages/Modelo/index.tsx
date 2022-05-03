8cce9aaaa398ca919a139ff357cbe995cee50ea8
TailonNecke <82898005+TailonNecke@users.noreply.github.com>
2022-05-03 09:08:50 -0300

    first commit

---
+++ src/Pages/Modelo/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container} from './styles';
+
+const Modelo: React.FC = () => {
+    
+
+
+    return (
+        <>
+            <Container>
+            
+            </Container>
+        </>
+
+    );
+}
+export default Modelo;
+
---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Switch } from "react-router";
+import Modelo from "../Pages/Modelo/index"
+
+
+const Routes: React.FC = () => (
+    <Switch>
+        <Modelo></Modelo>
+    </Switch>
+);
+
+export default Routes;
